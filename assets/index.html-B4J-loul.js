37a1ed12d1022dae97a9a7ae90050c30586df582
elsiosanchez <45974454+elsiosanchez@users.noreply.github.com>
2024-03-05 20:45:38 +0000

    Deploying to gh-pages from @ elsiosanchez/vue-ui-theme@32e952914d3b9039c225707d162037ef16e454f2 🚀

---
+++ assets/index.html-B4J-loul.js
@@ -0,0 +1,31 @@
+import{_ as o}from"./plugin-vue_export-helper-DlAUqK2U.js";import{o as n,c as r,a as i,b as e,d as t}from"./app-CNO2LRzS.js";const s={},a=e("p",null,[t("ADempiere-Vue is a new UI for ADempiere ERP, CRM & SCM. It is based on Vue"),e("i",{class:"fa-brands fa-vuejs",style:{color:"#63E6BE"}}),t(" , Nuxt and uses the UI Toolkit Element-Plus. It is a great UI for ADempiere ERP, CRM & SCM, based on the newest development stack of Vue, built-in i18n solution, typical templates for enterprise applications, lots of awesome features. This project was forked from Vue-Element-Admin")],-1);function m(p,l){return n(),r("div",null,[a,i(` 
+## 🚀Usage
+
+::: code-tabs#language
+
+@tab TS
+
+\`\`\`ts title=".vuepress/config.ts"
+import { defineUserConfig } from "vuepress";
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default defineUserConfig({
+  theme: hopeTheme({
+    // your theme config here
+  }),
+});
+\`\`\`
+
+@tab JS
+
+\`\`\`js title=".vuepress/config.js"
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default {
+  theme: hopeTheme({
+    // your theme config here
+  }),
+};
+\`\`\`
+
+::: `)])}const d=o(s,[["render",m],["__file","index.html.vue"]]),h=JSON.parse('{"path":"/","title":"Home","lang":"en-US","frontmatter":{"home":true,"icon":"home","title":"Home","heroImage":"/logo.svg","heroText":"UI Definitions Docs","tagline":"The new UI for ADempiere ERP ✨","actions":[{"text":"Get Started","link":"/get-started/","icon":"rocket","type":"primary"},{"text":"Guide","icon":"book-open","link":"/guide/"}],"copyright":false,"footer":"MIT Licensed | Copyright © 2019-present Mr.Hope","description":"ADempiere-Vue is a new UI for ADempiere ERP, CRM & SCM. It is based on Vue , Nuxt and uses the UI Toolkit Element-Plus. It is a great UI for ADempiere ERP, CRM & SCM, based on t...","head":[["link",{"rel":"alternate","hreflang":"es-es","href":"https://github.com/adempiere/vue-ui-theme.github.io/es/"}],["meta",{"property":"og:url","content":"https://github.com/adempiere/vue-ui-theme.github.io/"}],["meta",{"property":"og:site_name","content":"Adempiere UI"}],["meta",{"property":"og:title","content":"Home"}],["meta",{"property":"og:description","content":"ADempiere-Vue is a new UI for ADempiere ERP, CRM & SCM. It is based on Vue , Nuxt and uses the UI Toolkit Element-Plus. It is a great UI for ADempiere ERP, CRM & SCM, based on t..."}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"en-US"}],["meta",{"property":"og:locale:alternate","content":"es-ES"}],["meta",{"property":"og:updated_time","content":"2024-03-01T15:32:16.000Z"}],["meta",{"property":"article:author","content":"Elsio Sanchez"}],["meta",{"property":"article:modified_time","content":"2024-03-01T15:32:16.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"Home\\",\\"description\\":\\"ADempiere-Vue is a new UI for ADempiere ERP, CRM & SCM. It is based on Vue , Nuxt and uses the UI Toolkit Element-Plus. It is a great UI for ADempiere ERP, CRM & SCM, based on t...\\"}"]]},"headers":[],"git":{"createdTime":1709065747000,"updatedTime":1709307136000,"contributors":[{"name":"Elsio Sanchez","email":"elsiosanches@gmail.com","commits":3}]},"readingTime":{"minutes":1.64,"words":491},"filePathRelative":"README.md","localizedDate":"February 27, 2024","excerpt":"<p>ADempiere-Vue is a new UI for ADempiere ERP, CRM &amp; SCM. It is based on  Vue<i class=\\"fa-brands fa-vuejs\\" style=\\"color: #63E6BE;\\"></i> , Nuxt and uses the UI Toolkit Element-Plus. It is a great UI for ADempiere ERP, CRM &amp; SCM, based on the newest development stack of Vue, built-in i18n solution, typical templates for enterprise applications, lots of awesome features. This project was forked from Vue-Element-Admin</p>","autoDesc":true}');export{d as comp,h as data};
